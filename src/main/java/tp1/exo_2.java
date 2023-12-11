04ea7118d1fbd186b3dadc5e9726d391a5f65a00
Anna RAACH <annagraph92@gmail.com>
2023-12-11 14:28:31 +0100

    tp1(exo_2) calcul le prix ttc

---
+++ src/main/java/tp1/exo_2.java
@@ -0,0 +1,13 @@
+package tp1;
+
+public class exo_2 {
+    public static void main(String[] args) {
+        float htc = 100;
+
+        double tva = 0.20;
+        double ttc = htc + (htc * tva);
+
+        System.out.println("Le prix TTC est de : " + ttc);
+    }
+}
+

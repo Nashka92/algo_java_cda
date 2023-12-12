c3904ee3a56a2274af4b78144b3905b1c4dc7145
Anna RAACH <annagraph92@gmail.com>
2023-12-12 14:15:07 +0100

    tp1(exo_5): chiffre romain

--- src/main/java/tp1/Exo_5.java
+++ src/main/java/tp1/Exo_5.java
@@ -1,4 +1,21 @@
 package tp1;
 
 public class Exo_5 {
+    public static int main(String[] args){
+        if (nombreRomain.length() > 15);
+        throw new IllegalAccessException("le nombre ne peux pas excerder 15 caracteres");
+
+        int nombreArabe = 0;
+
+        for(int index = 0; nombreRomain.length(); indexx ++){
+            if (nombreRomain.chartAt(index) =='I');
+            nombreArabe += 1;
+        } else if (nombreRomain.chartAt(index) == 'V'){
+            if (index > 0 && nombreRomain.chartAt(index -1) == 'I'){
+                nombreArabe +=3;
+            } else {
+                nombreArabe += 8;
+            }
+        }
+    }
 }
